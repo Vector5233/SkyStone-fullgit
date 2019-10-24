50e145989a10aeeaa606e69af7e09452dfd38827
Vector5233B <vector5233@gmail.com>
2019-10-24 13:26:42 -0400

    Jun Park's glorious first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ChassisTeleOpHangLyu.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="ChassisTeleOpHangLyu", group="TeamCode")
+
+public class ChassisTeleOpHangLyu extends OpMode {
+    DcMotor cM1, cM2, cM3, cM4, collectorM1, collectorM2;
+    //Servo
+    public void init(){
+
+    }
+    public void loop(){
+
+    }
+}
