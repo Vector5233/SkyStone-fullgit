56485cd6649d0049ca430f469f07a5edf8e2aa5c
Vector5233B <vector5233@gmail.com>
2019-10-29 13:55:19 -0400

    Jun Park's glorious first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="LiftTeleOp", group="TeamCode")
@@ -11,10 +12,19 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class LiftTeleOp extends OpMode {
     DcMotor rightLift, leftLift;
     Servo grabberServo, grabberExtender, grabberRotation;
-    public void init(){
+
+    public void init() {
+        rightLift = hardwareMap.dcMotor.get("rightLift");
+        leftLift = hardwareMap. dcMotor.get ("leftLift");
+
+        grabberServo = hardwareMap.servo.get ("grabberServo");
+        grabberRotation = hardwareMap.servo.get ("grabberRotation");
+        grabberExtender = hardwareMap.servo.get ("grabberExtender");
+        
 
     }
-    public void loop(){
+
+    public void loop() {
 
     }
 }
