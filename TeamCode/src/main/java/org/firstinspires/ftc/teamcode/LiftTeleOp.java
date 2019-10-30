394c1868e2523f1039a4642e97da4b9cac7d2e4a
Vector5233B <vector5233@gmail.com>
2019-10-30 12:42:47 -0400

    Jun Park's glorious first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
@@ -11,25 +11,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class LiftTeleOp extends OpMode {
     DcMotor rightLift, leftLift;
-    Servo grabberServo, grabberExtender, grabberRotation;
+    Servo grabberServo, deliveryServo, rotationServo;
 
     public void init() {
         rightLift = hardwareMap.dcMotor.get("rightLift");
         leftLift = hardwareMap. dcMotor.get ("leftLift");
 
         grabberServo = hardwareMap.servo.get ("grabberServo");
-        grabberRotation = hardwareMap.servo.get ("grabberRotation");
-        grabberExtender = hardwareMap.servo.get ("grabberExtender");
+        deliveryServo = hardwareMap.servo.get ("deliveryServo");
+        rotationServo = hardwareMap.servo.get ("rotationServo");
 
         rightLift.setDirection(DcMotor.Direction.FORWARD);
         leftLift.setDirection(DcMotorSimple.Direction.REVERSE);
 
         rightLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
-
 
 
     }
