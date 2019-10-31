06a4ce68c9ec1bd5638e89d518b02cc174092a5b
Vector5233B <vector5233@gmail.com>
2019-10-31 13:54:48 -0400

    Jun Park's glorious third commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -11,14 +12,15 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class LiftTeleOp extends OpMode {
     DcMotor rightLift, leftLift;
-    Servo grabberServo, deliveryServo, rotationServo;
+    Servo grabberServo, rotationServo;
+    CRServo deliveryServo;
 
     public void init() {
         rightLift = hardwareMap.dcMotor.get("rightLift");
         leftLift = hardwareMap. dcMotor.get ("leftLift");
 
         grabberServo = hardwareMap.servo.get ("grabberServo");
-        deliveryServo = hardwareMap.servo.get ("deliveryServo");
+        deliveryServo = hardwareMap.crservo.get ("deliveryServo");
         rotationServo = hardwareMap.servo.get ("rotationServo");
 
         rightLift.setDirection(DcMotor.Direction.FORWARD);
@@ -30,7 +32,30 @@ public class LiftTeleOp extends OpMode {
 
     }
 
-    public void loop() {
+    public void loop(){
+        setLiftMotors();
+        setGrabberMoters();
+    }
 
+    private void setLiftMotors()  {
+        rightLift.setPower(gamepad2.left_stick_y);
+        leftLift.setPower(gamepad2.left_stick_y);
+
+    }
+
+    private void setGrabberMoters() {
+        deliveryServo.setPower(gamepad2.right_stick_y);
+
+        if (gamepad2.right_bumper == true) {
+            grabberServo.setPosition(0);
+        }
+
+        if (gamepad2.left_bumper == true) {
+            grabberServo.setPosition(0.5);
+        }
+
+        if  (gamepad2.right_trigger >= 0.5) {
+            rotationServo.setPosition();
+        }
     }
 }
