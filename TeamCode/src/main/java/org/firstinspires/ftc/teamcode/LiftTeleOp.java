ebca6df4dd4af7c647a728b44a9dbd32b6c7c070
Vector5233B <vector5233@gmail.com>
2019-11-02 10:41:55 -0400

    Jun Park's unrare commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTeleOp.java
@@ -10,10 +10,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="LiftTeleOp", group="TeamCode")
 
-public class LiftTeleOp extends OpMode {
+public class LiftTeleOp extends OpMode {   // maybe extend TeleOp?  what are the differences?
     DcMotor rightLift, leftLift;
     Servo grabberServo, rotationServo;
     CRServo deliveryServo;
+
+    final int maxLift = 700;
+    final int minLift = 0;
 
     public void init() {
         rightLift = hardwareMap.dcMotor.get("rightLift");
@@ -38,12 +41,20 @@ public class LiftTeleOp extends OpMode {
     }
 
     private void setLiftMotors()  {
-        rightLift.setPower(gamepad2.left_stick_y);
-        leftLift.setPower(gamepad2.left_stick_y);
+        // TODO consider carefully what actions could harm the lift and how to avoid doing those things
+        if(minLift <= leftLift.getCurrentPosition() &&leftLift.getCurrentPosition() <= maxLift) {
+            rightLift.setPower(gamepad2.left_stick_y);
+            leftLift.setPower(gamepad2.left_stick_y);
+        }
 
     }
 
     private void setGrabberMoters() {
+        // TODO all delivery components should be named deliveryXXX NOT grabberXXX
+
+        boolean ifUnpressedRT = true;
+        boolean ifUnpressedLT = true;
+
         deliveryServo.setPower(gamepad2.right_stick_y);
 
         if (gamepad2.right_bumper == true) {
@@ -51,11 +62,35 @@ public class LiftTeleOp extends OpMode {
         }
 
         if (gamepad2.left_bumper == true) {
-            grabberServo.setPosition(0.5);
+            grabberServo.setPosition(0.5);  //TODO fix this value
         }
 
-        if  (gamepad2.right_trigger >= 0.5) {
-            rotationServo.setPosition();
+        if  (gamepad2.right_trigger >= 0.5 && ifUnpressedRT == true) {
+            if(rotationServo.getPosition() < 0.1) {
+                rotationServo.setPosition(0.5);
+                ifUnpressedRT = false;
+            }
+            else if(0.4 < rotationServo.getPosition() && rotationServo.getPosition() < 0.6) {
+                rotationServo.setPosition(1);
+                ifUnpressedRT = false;
+            }
+        }
+        else
+            ifUnpressedRT = true;
+
+        if (gamepad2.left_trigger >= 0.5 && ifUnpressedLT == true) {
+            if(0.9 < rotationServo.getPosition()) {
+                rotationServo.setPosition(0.5);
+                ifUnpressedLT = false;
+            }
+            else if(0.4 < rotationServo.getPosition() && rotationServo.getPosition() < 0.6) {
+                rotationServo.setPosition(0);
+                ifUnpressedLT = false;
+            }
+        }
+        else{
+            ifUnpressedLT = true;
         }
     }
+
 }
