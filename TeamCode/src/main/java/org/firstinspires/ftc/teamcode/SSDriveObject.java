7a7eed4015d7e330f0c20aeb4aba8952b5ea4aef
Vector5233B <vector5233@gmail.com>
2019-11-04 13:50:10 -0500

    Jun Park's unrare commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSDriveObject.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSDriveObject.java
@@ -2,6 +2,17 @@ package org.firstinspires.ftc.teamcode;
 
 public class SSDriveObject extends Object{
     public SSDriveObject(){
+        final double TICKS_PER_INCH = 1120.0 / (4 * 3.14159265358979323846264);
+        final double ROBOT_RADIUS = 10;
+        final double TOLERANCE = 2;
+        private ElapsedTime strafeTimeout;
+        private ElapsedTime driveTimeout;
+        private ElapsedTime diagonalTimeout;
+        private ElapsedTime turnTimeout;
+        private ElapsedTime cmTimeout;
+        private ElapsedTime lmTimeout;
+
+
 
     }
 }
