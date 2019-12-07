fe9226304744aae9b801d51ce1a5b833da48cdb5
Vector5233B <vector5233@gmail.com>
2019-12-07 16:16:26 -0500

    Merge branch 'master' of https://github.com/Vector5233/SkyStone-fullgit
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkyStoneTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSDriveObject.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SSDriveObject.java
@@ -391,10 +391,10 @@ public class SSDriveObject extends Object{
     }
 
     public void setTurnPowerAll(double power) {
-        frontLeft.setPower(-power);
-        frontRight.setPower(power);
-        backLeft.setPower(-power);
-        backRight.setPower(power);
+        frontLeft.setPower(power);
+        frontRight.setPower(-power);
+        backLeft.setPower(power);
+        backRight.setPower(-power);
     }
 
     public boolean outsideOfRange(double targetDegrees, double initialDegrees) {
@@ -486,18 +486,21 @@ public class SSDriveObject extends Object{
         setModeAll(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         setModeAll(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        opmode.telemetry.addData("degrees, end of turn", gyro.getIntegratedZValue());
+        opmode.telemetry.addData("degrees, start of turn", gyro.getIntegratedZValue());
         opmode.telemetry.update();
 
         if (ticks > 0) {
-            while ((frontLeft.getCurrentPosition() <= ticks) && opmode.opModeIsActive()) {
-                setTurnPowerAll(powerLimit);
-            }
-        } else if (ticks < 0) {
-            while ((frontLeft.getCurrentPosition() >= ticks) && opmode.opModeIsActive()) {
+            // positive (CCW) turn => left motor goes in (-) direction
+            while ((frontLeft.getCurrentPosition() >= -ticks) && opmode.opModeIsActive()) {
                 setTurnPowerAll(-powerLimit);
             }
+        } else if (ticks < 0) {
+            while ((frontLeft.getCurrentPosition() <= -ticks) && opmode.opModeIsActive()) {
+                setTurnPowerAll(powerLimit);
+            }
         }
+        opmode.telemetry.addData("degrees, end of turn", gyro.getIntegratedZValue());
+        opmode.telemetry.update();
 
         /*if (ticks > 0) {
             while((frontLeft.getCurrentPosition() <= ticks) && opmode.opModeIsActive()) {
